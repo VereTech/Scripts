49e1e947b5b1908f44f44b36f2b03b5bba4f6b71
Edward Crocombe <edward@vere-tech.net.au>
2016-07-29 18:30:58 +1000

    Create GetLocalAccountsList
    get a list of local accounts on the machine the script is ran from

---
+++ Tasks/GetLocalAccountsList.ps1
@@ -0,0 +1,44 @@
+﻿
+<#
+  .SYNOPSIS
+  Report a list of local user accounts
+  .DESCRIPTION
+  Use by itself to obtain a list of the localhost
+  .EXAMPLE
+  Get-LocalAccountList
+  #>
+$ExitCode = "0"
+$users = ""
+$users = get-wmiobject win32_useraccount –computername $computer
+    
+if (!($users)) {
+
+    Write-Host "Unable to obtain a list of local users for $computer"
+        
+    if ($ExitCode -eq "0") {
+
+        $ExitCode = "1001"
+
+    }
+} else {
+
+    foreach ($user in $users) {
+    
+        Write-Host $user.Name
+        
+    }
+
+}
+
+
+if ($ExitCode -eq "0") {
+
+    exit 0
+
+} else {
+
+    Write-Host "Unable to obtain a list of local users"
+    exit 1001
+
+}
+
