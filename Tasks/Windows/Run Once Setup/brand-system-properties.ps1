05c8ef008d6390285df95c0a87a520c264d15734
Edward Crocombe <edward@vere-tech.net.au>
2016-07-30 21:00:19 +1000

    brand-system-properties.ps1
    Added logging

--- Tasks/Windows/Run Once Setup/brand-system-properties.ps1
+++ Tasks/Windows/Run Once Setup/brand-system-properties.ps1
@@ -16,8 +16,13 @@ param (
 [String]$OEMSupportURL = "https://www.vere-tech.net.au/",                                               # Website for support
 [String]$OEMLine1 = "Contact our Help Desk for computer problems",                                                     # Note to display to end users
 [String]$LogoURL = "https://www.vere-tech.net.au/images/sysprop.bmp"                                                      # The URL to .bmp or .jpg Logo
-
 )
+$logfile = "$env:SystemDrive\VereTech\Log.txt"
+$date = date
+Write-Output "--------------------------------------------------------------------------------------------------" | Out-File -Append $logfile
+Write-Output "$date : Starting 'branding system properties.ps1'" | Out-File -Append $logfile
+
+#Write-Output "$date : " | Out-File -Append $logfile
 
 Try {
 
@@ -28,25 +33,22 @@ Try {
 $Logooutput = "$env:SystemDrive\Windows\System32\oobe\sysprop.bmp" #for example C:\Users\User1\Desktop\logo1.jpg
 $OEMInformation = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation"
 
-
 #
 # Execution
 #
 
-
-
 # Download and format logo and store it
-
-
-
+Write-Output "$date : Loading System.Drawing" | Out-File -Append $logfile
 [reflection.assembly]::LoadWithPartialName("System.Drawing") | Out-Null
 
-    if (!(Test-Path "$Logooutput")) {
 
+    if (!(Test-Path "$Logooutput")) {
+        
+        Write-Output "$date : Downloading $LogoURL to $Logooutput" | Out-File -Append $logfile
         Start-BitsTransfer -Source "$LogoURL" -Destination "$Logooutput"
 
     }
-
+    Write-Output "$date : Re-Drawing $Logooutput" | Out-File -Append $logfile
     $Item = new-object System.Drawing.Bitmap $Logooutput
     $NewBitmap = new-object System.Drawing.Bitmap 110,110
     $g=[System.Drawing.Graphics]::FromImage($NewBitmap)
@@ -62,10 +64,11 @@ $OEMInformation = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformatio
 
 if (!(Test-Path "$OEMInformation")) {
 
+    Write-Output "$date : Creating $OEMInformation" | Out-File -Append $logfile
     New-Item -Path $OEMInformation
 
 }
-
+Write-Output "$date : Creating OEM Registry Values under $OEMInformation" | Out-File -Append $logfile
 Set-ItemProperty "$OEMInformation" -name "Manufacturer" -value "$OEMManufacturer"
 Set-ItemProperty "$OEMInformation" -name "SupportHours" -value "$OEMSupportHours"
 Set-ItemProperty "$OEMInformation" -name "SupportPhone" -value "$OEMSupportPhone"
@@ -83,67 +86,80 @@ $Query6 = Get-ItemPropertyValue "$OEMInformation" -name "Logo"
 $ChecksPassed = 0
 if ($Query1 -eq $OEMManufacturer) {
 
+    Write-Output "$date : Successfully set '$OEMManufacturer' as Manaufacturer" | Out-File -Append $logfile
     $ChecksPassed++
 
     } else {
-
-    Write-Host "Failed to set $OEMManufacturer as Manufacturer"
+    Write-Output "$date : Failed to set '$OEMManufacturer' as Manaufacturer" | Out-File -Append $logfile
+    Write-Host "Failed to set '$OEMManufacturer' as Manufacturer"
 
 }
 
 if ($Query2 -eq $OEMSupportHours) {
 
+    Write-Output "$date : Successfully set '$OEMSupportHours' as SupportHours" | Out-File -Append $logfile
     $ChecksPassed++
 
     } else {
 
-    Write-Host "Failed to set $OEMSupportHours as SupportHours"
+    Write-Output "$date : Failed to set '$OEMSupportHours' as SupportHours" | Out-File -Append $logfile
+    Write-Host "Failed to set '$OEMSupportHours' as SupportHours"
 
 }
 
 if ($Query3 -eq $OEMSupportPhone) {
 
+    Write-Output "$date : Successfully set '$OEMSupportPhone' as SupportPhone" | Out-File -Append $logfile
     $ChecksPassed++
 
     } else {
 
-    Write-Host "Failed to set $OEMSupportPhone as SupportPhone"
+    Write-Output "$date : Failed to set '$OEMSupportPhone' as SupportPhone" | Out-File -Append $logfile
+    Write-Host "Failed to set '$OEMSupportPhone' as SupportPhone"
 
 }
 
 if ($Query4 -eq $OEMSupportURL) {
 
+    Write-Output "$date : Successfully set '$OEMSupportURL' as SupportURL" | Out-File -Append $logfile
     $ChecksPassed++
 
     } else {
 
-    Write-Host "Failed to set $OEMSupportURL as SupportURL"
+    Write-Output "$date : Failed to set '$OEMSupportURL' as SupportURL" | Out-File -Append $logfile
+    Write-Host "Failed to set '$OEMSupportURL' as SupportURL"
 
 }
 
-if ($Query5 -eq $OEMLine) {
+if ($Query5 -eq $OEMLine1) {
 
+    Write-Output "$date : Successfully set '$OEMLine1' as Line1" | Out-File -Append $logfile
     $ChecksPassed++
 
     } else {
 
-    Write-Host "Failed to set $OEMLine as Line1"
+    Write-Output "$date : Failed to set '$OEMLine1' as Line1" | Out-File -Append $logfile
+    Write-Host "Failed to set '$OEMLine1' as Line1"
 
 }
 
 if ($Query6 -eq $name) {
 
+    Write-Output "$date : Successfully set '$name' as Logo" | Out-File -Append $logfile
     $ChecksPassed++
 
     } else {
 
-    Write-Host "Failed to set $name as Logo"
+    Write-Output "$date : Failed to set '$name' as Logo" | Out-File -Append $logfile
+    Write-Host "Failed to set '$name' as Logo"
 
 }
 
 if ($ChecksPassed -eq 6) {
 
-    Write-Host "This system has been branded as $OEMManufacturer"
+    Write-Output "$date : This system has been branded as '$OEMManufacturer'" | Out-File -Append $logfile
+
+    Write-Host "This system has been branded as '$OEMManufacturer'"
     Write-Host "Manufacturer  = $Query1"
     Write-Host "Support Hours = $Query2"
     Write-Host "Support Phone = $Query3"
@@ -153,6 +169,8 @@ if ($ChecksPassed -eq 6) {
     exit 0
 
 } else {
+
+    Write-Output "$date : $ChecksPassed out of 6 branding options where applied" | Out-File -Append $logfile
 
     Write-Host "$ChecksPassed out of 6 branding options where applied"
     Write-Host "Manufacturer  = $Query1"
@@ -168,7 +186,8 @@ if ($ChecksPassed -eq 6) {
 
 
 Catch {
-
+    
+    Write-Output "$date : Something went wrong..." | Out-File -Append $logfile
     Write-Host "Something went wrong..."
     exit 1001
 
